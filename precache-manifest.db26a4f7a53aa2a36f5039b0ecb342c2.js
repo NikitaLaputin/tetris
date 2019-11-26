4338e5bfe203052a5595dc1dc17caef7423cc9de
Nikita Laputin <laputinnik93@gmail.com>
2019-11-26 17:12:08 +0300

    Updates

---
+++ precache-manifest.db26a4f7a53aa2a36f5039b0ecb342c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ec4adfcbfb44156ad57d697e46c533",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "6956eadf30260e5ccebb",
+    "url": "/tetris/static/css/main.92c630e8.chunk.css"
+  },
+  {
+    "revision": "d23d2d8363d2685f1ded",
+    "url": "/tetris/static/js/2.5a6ef5e7.chunk.js"
+  },
+  {
+    "revision": "6956eadf30260e5ccebb",
+    "url": "/tetris/static/js/main.f53a5a74.chunk.js"
+  },
+  {
+    "revision": "96caf6de5370c7eb0c2c",
+    "url": "/tetris/static/js/runtime-main.5086e28a.js"
+  }
+]);
